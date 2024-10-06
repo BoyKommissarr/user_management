8d8d9139fa9ab8d9a9a550e14401efd717394d20
Abid Shaikh <shaikhabid2104@gmail.com>
2024-10-06 10:00:07 +0530

    add search path

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,6 +8,13 @@ class TeamsController < ApplicationController
     @team = Team.find(params[:id])
   end
 
+  def search
+    # params[:query]
+    # => "abid"
+    # Book.where("title LIKE ?", Book.sanitize_sql_like(params[:title]) + "%")
+    @result = User.where("name LIKE ?", User.sanitize_sql_like(params[:query]) + "%")
+  end
+
   private
   def team_params
     params.require(:team).permit(:id, :name)
