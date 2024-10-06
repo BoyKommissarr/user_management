70b74fe3b7030c2ccdf863e0fa2bec3ce1eb791b
Abid Shaikh <shaikhabid2104@gmail.com>
2024-10-06 09:16:00 +0530

    Teams CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "home#index"
+
+  resources :teams
+
+  resources :users
 end
